50ddf80da296e185c4250521d3c7305c7ad34ecb
Katie Lin <katie.lin@softwire.com>
2025-01-22 14:16:36 +0000

    remove errored orders from queue

--- scheduled_jobs.py
+++ scheduled_jobs.py
@@ -43,11 +43,12 @@ def process_orders(app):
             save_order(order)
         except:
             app.logger.exception("Error processing order {id}".format(id = order.id))
-
+            order.set_as_errored()
+            save_order(order)
 
 
 def get_queue_of_orders_to_process():
     allOrders = get_all_orders()
-    queuedOrders = filter(lambda order: order.date_processed == None, allOrders)
+    queuedOrders = filter(lambda order: order.date_processed == None and order.status != "Errored", allOrders)
     sortedQueue = sorted(queuedOrders, key= lambda order: order.date_placed)
     return list(sortedQueue)
